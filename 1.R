3e6adb5233d15964bba05d112800eef74d58ab11
Kinizsi <212442415@logon.ds.ge.com>
2017-09-23 13:48:53 +0200

    math operators + constants

--- 1.R
+++ 1.R
@@ -1,1 +1,15 @@
 1+3
+2*3
+pi
+'pi'
+"pi"
+str(pi)
+letters
+str(letters)
+LETTERS
+letters[1]
+letters[10]
+c(1,10)
+letters[c(1,10)]
+letters[c(     1  ,    10)] #comment - spacek nem számítanak, de rakjunk bele a műveletek és veszzők után
+
